b144f3bdfc6aee25e1cbab16e262f2f2ad166e21
Clement Lakhal <clementlakhal@gmail.com>
2019-05-12 04:20:54 +0200

    Remove useless stuff

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
+
+import './app.scss';
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Hello!</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
