caff9995c48ccaf690d672d9fb01899daf6dd58b
Clement Lakhal <clementlakhal@gmail.com>
2019-05-13 08:55:21 +0200

    Update the Web3 wrapper to support Ganache local network

--- src/components/web3Wrapper/connectors.js
+++ src/components/web3Wrapper/connectors.js
@@ -4,7 +4,7 @@ import {
 
 const { InjectedConnector } = Connectors;
 
-const MetaMask = new InjectedConnector({ supportedNetworks: [1, 3, 4] });
+const MetaMask = new InjectedConnector({ supportedNetworks: [1, 3, 4, 1557627200236] });
 
 const connectors = { MetaMask };
 
